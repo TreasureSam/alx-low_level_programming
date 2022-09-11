191c08554ea49efbc00438849cd9c63c1e8bce3f
TreasureSam <samuelomotayosamuel@gmail.com>
2022-09-10 21:10:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /** 
- * * main - Entry point 
- * * 
- * * Return: Always 0 (Success) 
- * */
+ * Description: main - prints out a string to stdout. 
+ * Return: 0 if Success.
+ */ 
 int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle"); return (0);
