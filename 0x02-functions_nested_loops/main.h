ef7cd06c3092b48575ac44e173b4400d5effea19
TreasureSam <samuelomotayosamuel@gmail.com>
2022-09-14 20:28:41 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int n1, int n2);
+void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
