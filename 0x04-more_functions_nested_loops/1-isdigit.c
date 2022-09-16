dfd4f17d583798b0608c137f24a2ecc19d1b0ff3
TreasureSam <samuelomotayosamuel@gmail.com>
2022-09-15 21:05:25 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * _isupper - checks is a character is uppercase
- *  * @c: carrier integer variable
+/* *
+ * _isdigit - checks if a character is digit
+ *  * @c: carrier variable
  *   * Return: 1 if true, 0 if false
- */
-
-int _isupper(int c)
+ *   
+ * */
+int _isdigit(int c)
 {
-	 char c;
-
-	     c = 'A';
-	         printf("%c: %d\n", c, _isupper(c));
-		     c = 'a';
-		         printf("%c: %d\n", c, _isupper(c));
-			     return (0)
+ c = '0';
+ printf("%c: %d\n", c, _isdigit(c));
+ c = 'a';
+ printf("%c: %d\n", c, _isdigit(c));
+ return (0);
 }
