95676a98d24d0e70528440a16a1fa17524c9e4df
TreasureSam <samuelomotayosamuel@gmail.com>
2022-09-15 20:53:29 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks is a character is uppercase
+ *  * @c: carrier integer variable
+ *   * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	 char c;
+
+	     c = 'A';
+	         printf("%c: %d\n", c, _isupper(c));
+		     c = 'a';
+		         printf("%c: %d\n", c, _isupper(c));
+			     return (0)
+}
